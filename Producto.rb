75f2b0922a0589b75196ab5681e85750de752e65
Alejandro Tarazona <31288964+Bepoom@users.noreply.github.com>
2018-09-06 00:30:52 -0500

    Add files via upload

--- Producto.rb
+++ Producto.rb
@@ -1,31 +1,42 @@
 require "./promocion"
 class Producto
 
-  def initialize(codigo_producto, nombre, precio, unidad, estoyEnPromocion)
-    @codigo_producto = codigo_producto
-    @nombre = nombre
-    @precio = precio
-    @unidad = unidad
-    @estoyEnPromocion = estoyEnPromocion
+  attr_accessor :codigo
+  attr_accessor :nombre
+  attr_accessor :precio
+  attr_accessor :unidad
+  def initialize(params)
+    @codigo_producto = params[:codigo_producto]
+    @nombre = params[:nombre]
+    @precio = params[:precio]
+    @unidad = params[:unidad]
+    @estoyEnPromocion = params[:estoyEnPromocion]
   end
 
   def codigoProducto
     @codigoProducto
   end
+
   def nombre
     @nombre
   end
+
   def precio
     @precio
   end
+
   def unidad
     @unidad
   end
+
   def estoyEnPromocion
     @estoyEnPromocion
   end
-  def crearPromocion()
 
+  def crearPromocion
+    if estoyEnPromocion
+      promocion.initialize
+    else
   end
 
   def calcularPrecioProducto(precio)
@@ -35,5 +46,4 @@ class Producto
   def to_string
     return "Codigo Producto: #{codigo_producto}, nombre: #{nombre}, precio: #{precio}, unidad: #{unidad}, ¿tiene promocion?: #{estoyEnPromocion.t_} "
   end
-
 end
