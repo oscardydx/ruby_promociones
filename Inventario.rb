6192e0e6eb48aaff433a72e7e80196abf60a7ad5
KetheShippeo <valery-rod@hotmail.com>
2018-09-05 23:57:46 -0500

    InventarioBeta0.1

--- Inventario.rb
+++ Inventario.rb
@@ -1,10 +1,3 @@
-require "./producto"
 class Inventario
-  listaProductos=Array.new
-  #{:codigo,:nombre,:precio,:unidad}
-  def registrarProducto(*args)
-    puts 'entre :3'
-  end
-end
 
-puts 'hola Inventario'
+end
