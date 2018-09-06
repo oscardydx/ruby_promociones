fb9eb5eccc87a5a86344a04c454d74eabd7df83b
KetheShippeo <valery-rod@hotmail.com>
2018-09-06 00:08:33 -0500

    ProductoBeta0.1

---
+++ Producto.rb
@@ -0,0 +1,41 @@
+require "./promocion"
+class Producto
+
+  def initialize(codigo_producto, nombre, precio, unidad, estoyEnPromocion)
+    @codigo_producto = codigo_producto
+    @nombre = nombre
+    @precio = precio
+    @unidad = unidad
+    @estoyEnPromocion = estoyEnPromocion
+  end
+
+  def codigoProducto
+    @codigoProducto
+  end
+  def nombre
+    @nombre
+  end
+  def precio
+    @precio
+  end
+  def unidad
+    @unidad
+  end
+  def estoyEnPromocion
+    @estoyEnPromocion
+  end
+  def crearPromocion()
+    if estoyEnPromocion
+      promocion.initialize
+    else
+  end
+
+  def calcularPrecioProducto(precio)
+
+  end
+
+  def to_string
+    return "Codigo Producto: #{codigo_producto}, nombre: #{nombre}, precio: #{precio}, unidad: #{unidad}, ¿tiene promocion?: #{estoyEnPromocion.t_} "
+  end
+
+end
